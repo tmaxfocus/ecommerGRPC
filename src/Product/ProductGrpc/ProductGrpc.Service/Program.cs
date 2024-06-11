4e3c7821d693d2409292426e74cef01756257acb
tobi adeogun <tobiadeomax@outlook.com>
2024-06-11 09:44:14 +0100

    worked on product service

--- src/Product/ProductGrpc/ProductGrpc.Service/Program.cs
+++ src/Product/ProductGrpc/ProductGrpc.Service/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ProductGrpc.Service.Data;
+using ProductGrpc.Service.Services;
 //using ProductGrpc.Service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,7 +16,7 @@ ProductsContextSeed.seedAsync(myService);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//app.MapGrpcService<GreeterService>();
+app.MapGrpcService<ProductService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ src/Product/ProductGrpc/ProductGrpc.Service/Services/ProductService.cs
@@ -0,0 +1,48 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using ProductGrpc.Protos;
+using ProductGrpc.Service.Data;
+
+namespace ProductGrpc.Service.Services
+{
+	public class ProductService : ProductProtoService.ProductProtoServiceBase
+	{
+		private readonly ProductsContext _productContext;
+		private readonly ILogger<ProductService> _logger;
+
+		public ProductService(ILogger<ProductService> logger, ProductsContext productContext)
+		{
+			_logger = logger;
+			_productContext = productContext;
+		}
+
+		public override Task<Empty> Test(Empty request, ServerCallContext context)
+		{
+			return base.Test(request, context);
+		}
+
+		public override async Task<ProductModel> GetProduct(GetProductRequest request, ServerCallContext context)
+		{	
+			var product = await _productContext.Products.FindAsync(request.ProductId);
+
+			if (product == null) 
+			{
+				// throw an rpc exception
+			}
+
+			var productModel = new ProductModel
+			{
+				ProductId = product.ProductId,
+				Name = product.Name,
+				Description = product.Description,
+				Price = product.Price,
+				Status = ProductStatus.Instock,
+				CreatedTime = Timestamp
+				.FromDateTime(product.CreatedTime)
+			};
+
+			return productModel;
+			//return base.GetProduct(request, context);
+		}
+	}
+}
